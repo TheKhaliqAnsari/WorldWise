e6c75fa5e38b8251a7f1db147cf46380ef2a2815
TheKhaliqAnsari <khaliqansari771@gmail.com>
2024-06-08 01:47:38 +0530

    Completed the Navigation UI Now started working on Map functionality and programmatic navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,55 @@
-import {BrowserRouter, Routes, Route}  from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
+import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
+import CountriesList from "./components/CountryList";
+import City from "./components/City"
+const BASE_URL = 'http://localhost:8000';
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchCities(){
+      try{
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      }catch(error){
+        alert(error);
+      }finally{
+        setIsLoading(false);
+      }
+    }
+
+    fetchCities();
+  }, [])
 
   return (
-    <BrowserRouter >
-      <Routes >
-        <Route exact path="/" element={<Homepage/>} />
-        <Route path="product" element={<Product/>} />
-        <Route path="pricing" element={<Pricing/>} />
-        <Route path="login" element={<Login/>} />
-        <Route path="app" element={<AppLayout />} />
-        <Route path="*" element={<PageNotFound/>} />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<CityList cities={cities} isLoading={isLoading}/>} />
+          <Route path="cities" element={<CityList cities={cities} isLoading={isLoading}/>} />
+          <Route path="cities/:id" element={<City />} />
+          <Route path="countries" element={<CountriesList cities={cities} isLoading={isLoading}/>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
---
+++ src/components/CountryList.jsx
@@ -0,0 +1,28 @@
+import CountryItem from "./CountryItem";
+import styles from "./CountryList.module.css";
+import Spinner from "./Spinner";
+import Message from "./Message";
+
+function CountriesList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+  if (!cities.length)
+    return <Message message="Add Your first city by clicking on Map" />;
+
+  // console.log(cities)
+
+  const countries = cities.reduce((arr, city) => {
+    if (!arr.map((ele) => ele.country).includes(city.country))
+      return [...arr, { country: city.country, emoji: city.emoji }];
+    else return arr;
+  }, []);
+
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country,idx) => {
+        return <CountryItem country={country} key={idx} />;
+      })}
+    </ul>
+  );
+}
+
+export default CountriesList;
